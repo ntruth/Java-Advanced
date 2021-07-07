992b564469ec6cd58410a5047dab8909dbb9717b
ann <2634817136@qq.com>
2021-07-08 07:34:59 +0800

    优化导入

--- 4-middleware/redis/redis/src/main/java/com/sduty/redis/config/JedisConfig.java
+++ 4-middleware/redis/redis/src/main/java/com/sduty/redis/config/JedisConfig.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
